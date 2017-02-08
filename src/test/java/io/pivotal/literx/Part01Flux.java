75cefb47e1f73ba6edab3710ef9ddecd94a40ec0
Zbigniew Tomczak <zbigniew.tomczak@pl.abb.com>
2017-02-08 15:24:58 +0100

    Part 1 Flux

--- src/test/java/io/pivotal/literx/Part01Flux.java
+++ src/test/java/io/pivotal/literx/Part01Flux.java
@@ -1,9 +1,9 @@
 package io.pivotal.literx;
 
-import java.time.Duration;
 import java.util.Arrays;
 
 import org.junit.Test;
+
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -25,9 +25,8 @@ public class Part01Flux {
 				.verifyComplete();
 	}
 
-	// TODO Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 //========================================================================================
@@ -40,9 +39,8 @@ public class Part01Flux {
 				.verifyComplete();
 	}
 
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo", "bar");
 	}
 
 //========================================================================================
@@ -55,9 +53,8 @@ public class Part01Flux {
 				.verifyComplete();
 	}
 
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		return Flux.fromIterable(Arrays.asList("foo", "bar"));
 	}
 
 //========================================================================================
@@ -68,9 +65,8 @@ public class Part01Flux {
 		StepVerifier.create(flux)
 				.verifyError(IllegalStateException.class);
 	}
-	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(new IllegalStateException());
 	}
 
 //========================================================================================
@@ -83,9 +79,8 @@ public class Part01Flux {
 				.verifyComplete();
 	}
 
-	// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
 	Flux<Long> counter() {
-		return null;
+		return Flux.intervalMillis(100).take(10);
 	}
 
 }
